d1fbe01c237beff5018b337db11fc02177e2b735
Gawesha <gawesharg21@gmail.com>
2025-03-30 22:36:38 +0530

    Delete Day 5/example directory

--- Day 5/example/Db/Students.js
+++
@@ -1,9 +0,0 @@
-let students = [
-    {regNo:'2021ICT28', name:'Gawesha',gender:'female',age:25,course:'IT'},
-    {regNo:'2021ICT111', name:'nisala',age:23,gender:'male',course:'Bio'},
-    {regNo:'2021ICT12', name:'nishmi',age:22,gender:'female',course:'Tec'},
-    {regNo:'2021ICT88', name:'sandaru',age:22,gender:'female',course:'Tec'},
-    {regNo:'2021ICT91', name:'kaveesh',age:20,gender:'male',course:'IT'}
-]
-
-module.exports = students;
